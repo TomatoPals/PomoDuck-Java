6db0f8e225a6ef06838289b2fe9ae350bafe81ad
Michael Curtis <mpbcurtis@gmail.com>
2021-06-16 16:01:10 -0400

    base app

---
+++ src/main/java/com/tomatopals/pomoduckjava/PomoduckJavaApplication.java
@@ -0,0 +1,13 @@
+package com.tomatopals.pomoduckjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomoduckJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomoduckJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomatopals/pomoduckjava/PomoduckJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomatopals.pomoduckjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomoduckJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
